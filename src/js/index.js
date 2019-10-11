b029425a2d8926872e98ed2d8502ce358bd0ed90
darthbel <febelgine@gmail.com>
2019-10-10 20:25:17 -0400

    finish Recipe model

--- src/js/index.js
+++ src/js/index.js
@@ -27,12 +27,17 @@ const controlSearch = async () => {
     searchView.clearResults()
     renderLoader(elements.searchRes)
 
-    // 4 - Search for recipes
-    await state.search.getResults()
+    try {
+      // 4 - Search for recipes
+      await state.search.getResults()
 
-    // 5 - Render results on UI
-    clearLoader()
-    searchView.renderResults(state.search.result)
+      // 5 - Render results on UI
+      clearLoader()
+      searchView.renderResults(state.search.result)
+    } catch (err) {
+      alert('Something wrong with the search...')
+      clearLoader()
+    }
   }
 }
 
@@ -54,6 +59,32 @@ elements.searchResPages.addEventListener('click', e => {
 /**
  * RECIPE CONTROLLER
  */
-const r = new Recipe(46956)
-r.getRecipe()
-console.log(r)
+const controlRecipe = async () => {
+  // Get ID from url
+  const id = window.location.hash.replace('#', '')
+  console.log(id)
+
+  if (id) {
+    // Prepare UI for changes
+
+    // Create new recipe object
+    state.recipe = new Recipe(id)
+
+    try {
+      // Get recipes data and parse ingredients
+      await state.recipe.getRecipe()
+      state.recipe.parseIngredients()
+
+      // Calculate servings and time
+      state.recipe.calcTime()
+      state.recipe.calcServings()
+
+      // Render recipe
+      console.log(state.recipe)
+    } catch (err) {
+      alert('Error processing recipe!')
+    }
+  }
+}
+
+;['hashchange', 'load'].forEach(e => window.addEventListener(e, controlRecipe))
